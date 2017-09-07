d34ccd4018f1a64d9b83ec08e3d36aea88aa88a0
Ivan Ken Tiu <ivankentiu@Ivans-MacBook-Pro.local>
2017-09-07 16:11:46 +0800

    initial view created

---
+++ appstore1/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  appstore1
+//
+//  Created by Ivan Ken Tiu on 07/09/2017.
+//  Copyright © 2017 Ivan Ken Tiu. All rights reserved.
+//
+
+import UIKit
+
+class FeaturedAppsController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = UIColor.white
+    }
+
+
+}
+
