29541e5506fd765c109a7ce157bbfa1d3ef7bc7c
Ivan Ken Tiu <ivankentiu@Ivans-MacBook-Pro.local>
2017-09-09 11:48:29 +0800

    Finish Proto 1

--- appstore1/CategoryCell.swift
+++ appstore1/CategoryCell.swift
@@ -21,6 +21,16 @@ class CategoryCell: UICollectionViewCell, UICollectionViewDataSource, UICollecti
         fatalError("init(coder:) has not been implemented")
     }
     
+    //label of the categories
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Best New Apps"
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    
     //create collection view
     let appsCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
@@ -38,6 +48,13 @@ class CategoryCell: UICollectionViewCell, UICollectionViewDataSource, UICollecti
         return collectionView
     }()
     
+    let dividerLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0.4, alpha: 0.4)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
   
     
     func setupViews() {
@@ -45,6 +62,8 @@ class CategoryCell: UICollectionViewCell, UICollectionViewDataSource, UICollecti
         
         //add collection view unto the sub view hierarchy of this cell
         addSubview(appsCollectionView)
+        addSubview(dividerLineView)
+        addSubview(nameLabel)
         
         //add the protocols above
         appsCollectionView.dataSource = self
@@ -55,10 +74,14 @@ class CategoryCell: UICollectionViewCell, UICollectionViewDataSource, UICollecti
         
         //use layout contraints to plop view into my cell
         //expand horizontally from the left to the right edge v-0 being our appsCollectionView (-8- padding)
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-14-[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-14-[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": dividerLineView]))
+        
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": appsCollectionView]))
         
         //and the same for Vertical axis top to bottom
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": appsCollectionView]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[nameLabel(30)][v0][v1(0.5)]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["nameLabel": nameLabel, "v0": appsCollectionView, "v1": dividerLineView]))
         
     }
     
@@ -72,9 +95,14 @@ class CategoryCell: UICollectionViewCell, UICollectionViewDataSource, UICollecti
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 100, height: frame.height)
+        //bash on height of collection view
+        return CGSize(width: 100, height: frame.height - 32) //because of [nameLabel(30)][v1(0.5)], image cut off so -32
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        //push left right edge by couple of pixels (margin)
+        return UIEdgeInsetsMake(0, 14, 0, 14)
+    }
   
     
 }
@@ -90,7 +118,49 @@ class AppCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named : "monster")
+        iv.contentMode = .scaleAspectFill //maintains the aspect ratio of the image
+        iv.layer.cornerRadius = 16
+        iv.layer.masksToBounds = true // turned on by default?
+        return iv
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Cool Build It: Monster"
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    let categoryLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Entertainment"
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.textColor = UIColor.darkGray
+        return label
+    }()
+    
+    let priceLabel: UILabel = {
+        let label = UILabel()
+        label.text = "$3.99"
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.textColor = UIColor.darkGray
+        return label
+    }()
+
     func setupViews() {
-        backgroundColor = UIColor.black
+        
+        addSubview(imageView)
+        addSubview(nameLabel)
+        addSubview(categoryLabel)
+        addSubview(priceLabel)
+        
+        imageView.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.width)
+        nameLabel.frame = CGRect(x: 0, y: frame.width + 2, width: frame.width, height: 40)
+        categoryLabel.frame = CGRect(x: 0, y: frame.width + 38, width: frame.width, height: 20)
+        priceLabel.frame = CGRect(x: 0, y: frame.width + 56, width: frame.width, height: 20)
     }
 }
